94a734b65ce4fd42e8353b16bdf2222701793b8f
ILUHAAAA <pramskii.is@phystech.edu>
2025-01-07 11:57:11 +0300

    2 new methods for parsing with docling

--- parse.py
+++ parse.py
@@ -88,39 +88,25 @@ class Parser():
             case 'doc_with_images':
                 self.md = MarkItDown()
                 self.doc_converter = self._docling_parser()
-                self.convert = self.docx_with_img_parse
+                self.convert = self.docx_with_img
             case 'doc_text_only':
                 self.md = MarkItDown()
-                self.convert = self.docx_text_only_parse
+                self.convert = self.docx_text_only
             case 'pdf_with_text_layer':
                 self.megaparse = self._megaparse_parser()
-                self.convert = self.pdf_text_only_parse
+                self.convert = self.pdf_text_only
             case 'pdf_without_text_layer':
                 self.megaparse = self._megaparse_parser()
                 self.doc_converter = self._docling_parser()
-                self.convert = self.pdf_with_img_parse
+                self.convert = self.pdf_with_img
+            case 'pdf_with_images_docling':
+                self.doc_converter = self._docling_parser()
+                self.convert = self.pdf_with_img_docling
+            case 'pdf_text_only_docling':
+                self.doc_converter = self._docling_parser(img_flag=False)
+                self.convert = self.pdf_text_only_docling
 
-
-    def pdf_text_only_parse(self, name_of_file):
-        self.megaparse.load(f"./uploaded_files/{name_of_file}")
-
-        output_dir = Path(f"./processed_files/parsed_{name_of_file[:-4]}")
-        output_dir.mkdir(parents=True, exist_ok=True)
-        # Сохранение результата в Markdown
-        output_path = output_dir / f"{name_of_file[:-3]}md"
-        output_path = output_path.as_posix()
-        self.megaparse.save(output_path)
-        print(f"Результат сохранён в: {output_path}")
-
-
-    def pdf_with_img_parse(self, name_of_file):
-        response = self.megaparse.load(f"./uploaded_files/{name_of_file}")
-
-        # Сохранение результата в Markdown
-        # output_path = f"./processed_files/{name_of_file[:-3]}md"
-        # megaparse.save(output_path)
-        # print(f"Результат сохранён в: {output_path}")
-
+    def pdf_text_only_docling(self, name_of_file):
         input_doc_path = Path(f"./uploaded_files/{name_of_file}")
         output_dir = Path(f"./processed_files/parsed_{input_doc_path.name[:-4]}")
 
@@ -131,45 +117,81 @@ class Parser():
         temp_input_doc_path.unlink()
 
         output_dir.mkdir(parents=True, exist_ok=True)
-            
-        images_list = self._get_images(output_dir, conv_res.document)
 
-        md_filename = output_dir / f"{input_doc_path.name[:-3]}md"
+        md_file_path = output_dir / f"{input_doc_path.name[:-3]}md"
+        conv_res.document.save_as_markdown(md_file_path)
+# Добавить метод, который будет убирать пометки о наличии картинок
 
+    def pdf_with_img_docling(self, name_of_file):
+        input_doc_path = Path(f"./uploaded_files/{name_of_file}")
+        output_dir = Path(f"./processed_files/parsed_{input_doc_path.name[:-4]}")
+
+        temp_input_doc_path = input_doc_path.parent / 'temp_file.pdf'
+        shutil.copy(input_doc_path, temp_input_doc_path)
+
+        conv_res = self.doc_converter.convert(temp_input_doc_path)
+        temp_input_doc_path.unlink()
+
+        output_dir.mkdir(parents=True, exist_ok=True)
+
+        md_file_path = output_dir / f"{input_doc_path.name[:-3]}md"
         images_dir = output_dir / "images/"
         images_dir.mkdir(parents=True, exist_ok=True)
+        conv_res.document.save_as_markdown(md_file_path, artifacts_dir=images_dir, image_mode=ImageRefMode.REFERENCED)
 
-        result = self._insert_ref(response, images_list, pattern = r"!\[Image\][^\s]*", prefix='![Image]')
-        with open(md_filename, 'w', encoding='utf-8') as f:
-            f.write(result)   
-        
-    def docx_with_img_parse(self, name_of_file):
-        input_doc_path = Path(f"./uploaded_files/{name_of_file}")
-        output_dir = Path(f"./processed_files/parsed_{input_doc_path.name.rpartition('.')[0]}")
+    def pdf_text_only(self, name_of_file):
+        self.megaparse.load(f"./uploaded_files/{name_of_file}")
+
+        output_dir = Path(f"./processed_files/parsed_{name_of_file[:-4]}")
         output_dir.mkdir(parents=True, exist_ok=True)
+        # Сохранение результата в Markdown
+        md_file_path = output_dir / f"{name_of_file[:-3]}md"
+        md_file_path = md_file_path.as_posix()
+        self.megaparse.save(md_file_path)
 
-        conv_res = self.doc_converter.convert(input_doc_path)
-        
+    def pdf_with_img(self, name_of_file):
+        input_doc_path = Path(f"./uploaded_files/{name_of_file}")
+        output_dir = Path(f"./processed_files/parsed_{input_doc_path.name[:-4]}")
+        temp_input_doc_path = input_doc_path.parent / 'temp_file.pdf'
+        shutil.copy(input_doc_path, temp_input_doc_path)
+
+        response = self.megaparse.load(f"./uploaded_files/{name_of_file}")
+        conv_res = self.doc_converter.convert(temp_input_doc_path)
+        temp_input_doc_path.unlink()
+
+        output_dir.mkdir(parents=True, exist_ok=True)
         images_list = self._get_images(output_dir, conv_res.document)
 
-        md_filename = output_dir / f"{input_doc_path.name.rpartition('.')[0]}.md"
-
-        parsed_text = self.md.convert(input_doc_path.as_posix()).text_content
-        result = self._insert_ref(parsed_text, images_list, pattern=r"\(data:image\/([a-zA-Z]+);base64\.\.\.\)")
-
-        with open(md_filename, 'w', encoding='utf-8') as f:
-            f.write(result)        
-
-    def docx_text_only_parse(self, name_of_file):
+        result = self._insert_ref(response, images_list, pattern = r"!\[Image\][^\s]*", prefix='![Image]')
+        md_file_path = output_dir / f"{input_doc_path.name[:-3]}md"
+        with open(md_file_path, 'w', encoding='utf-8') as f:
+            f.write(result)   
+        
+    def docx_with_img(self, name_of_file):
         input_doc_path = Path(f"./uploaded_files/{name_of_file}")
         output_dir = Path(f"./processed_files/parsed_{input_doc_path.name.rpartition('.')[0]}")
-        output_dir.mkdir(parents=True, exist_ok=True)
 
-        md_filename = output_dir / f"{input_doc_path.name.rpartition('.')[0]}.md"
+        conv_res = self.doc_converter.convert(input_doc_path)
+        parsed_text = self.md.convert(input_doc_path.as_posix()).text_content
+        
+        output_dir.mkdir(parents=True, exist_ok=True)
+        images_list = self._get_images(output_dir, conv_res.document)
+
+        result = self._insert_ref(parsed_text, images_list, pattern=r"\(data:image\/([a-zA-Z]+);base64\.\.\.\)")
+        md_file_path = output_dir / f"{input_doc_path.name.rpartition('.')[0]}.md"
+        with open(md_file_path, 'w', encoding='utf-8') as f:
+            f.write(result)        
+
+    def docx_text_only(self, name_of_file):
+        input_doc_path = Path(f"./uploaded_files/{name_of_file}")
+        output_dir = Path(f"./processed_files/parsed_{input_doc_path.name.rpartition('.')[0]}")
 
         parsed_text = self.md.convert(input_doc_path.as_posix()).text_content
 
-        with open(md_filename, 'w', encoding='utf-8') as f:
+        output_dir.mkdir(parents=True, exist_ok=True)
+
+        md_file_path = output_dir / f"{input_doc_path.name.rpartition('.')[0]}.md"
+        with open(md_file_path, 'w', encoding='utf-8') as f:
             f.write(parsed_text)        
             
     def _insert_ref(self, text, images_list, pattern, prefix=''):
@@ -216,10 +238,11 @@ class Parser():
 
         return MegaParse(parser)
 
-    def _docling_parser(self):
+    def _docling_parser(self, img_flag=True):
         pipeline_options = PdfPipelineOptions()
-        pipeline_options.images_scale = IMAGE_RESOLUTION_SCALE
-        pipeline_options.generate_picture_images = True
+        if img_flag:
+            pipeline_options.images_scale = IMAGE_RESOLUTION_SCALE
+            pipeline_options.generate_picture_images = True
 
         return DocumentConverter(
                     format_options={
